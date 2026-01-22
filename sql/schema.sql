45bc5dfc576598b1578ce5142454d6db57505eab
IkerFerreiro <ikerferreiroclase@gmail.com>
2026-01-22 18:26:38 +0100

    Add 'clientes' table with initial data
    Create 'clientes' table and insert initial data.

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+USE prometeo;
+CREATE TABLE clientes (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nombre VARCHAR(50),
+ email VARCHAR(100)
+);
+INSERT INTO clientes(nombre, email)
+VALUES ('Ana Alonso', 'ana@prometeo.com');
+
+Luz María Álvarez Moreno
+18:22
+CREATE USER 
